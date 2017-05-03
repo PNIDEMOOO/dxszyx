f25a7fcea9a03ec37801326b362292762f04245c
PNIDEMOOO <452937660@qq.com>
2017-05-03 16:56:47 +0800

    add siderbar of list

--- js/map.js
+++ js/map.js
@@ -18,10 +18,10 @@ map.addControl(new BMap.MapTypeControl());   //添加地图类型控件
 map.setCurrentCity("上海");          // 设置地图显示的城市 此项是必须设置的
 map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
 
-//单击获取点击的经纬度
-map.addEventListener("click", function (e) {
-    alert(e.point.lng + "," + e.point.lat);
-});
+// //单击获取点击的经纬度
+// map.addEventListener("click", function (e) {
+//     alert(e.point.lng + "," + e.point.lat);
+// });
 
 $.ajax({
     type: "get",
